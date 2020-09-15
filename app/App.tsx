e467192377342582696497a7b73215bf81656fa5
Robert Sanins <rob.sanins@lovehoney.co.uk>
2020-09-15 10:02:37 +0100

    build home screen

--- app/App.tsx
+++ app/App.tsx
@@ -1,31 +1,12 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { HomeScreen } from './homeScreen/HomeScreen';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <ScrollView>
-        <Text>Some text</Text>
-        <View>
-          <Text>Some more text</Text>
-          <Image
-            source={{
-              uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-            }}
-            style={{ width: 200, height: 200 }}
-          />
-        </View>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1
-          }}
-          defaultValue="You can type in me"
-        />
-      </ScrollView>
+      <HomeScreen />
     </NavigationContainer>
   );
 }
